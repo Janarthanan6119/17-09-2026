b9e079e635ddd608149c0fe1bddddbc5819aed95
Janarthanan6119 <91175755+Janarthanan6119@users.noreply.github.com>
2026-09-17 16:02:40 +0530

    Add files via upload
    Solved a Question in C,C++,Pushed on 17/09/2026

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+int main(){
+    int path[] =     {800, 600, 750, 900, 1400, 1200, 1100, 1500};
+    string stops[] = {"TH","GA","IC","HA","TE","LU","NI","CA"};
+    string src,dest;
+    cin>>src>>dest;
+    cout<<src<<dest;
+    int start=0,end=0,found=0;
+    int length = sizeof(path)/sizeof(path[0]);
+    for(int i=0;i<length;i++){
+        if(src==stops[i]){
+            start=i;
+            found=1;
+        }
+        if(dest==stops[i]){
+            end=i;
+        }
+    }
+    cout<<"Start:"<<start<<"End:"<<end<<endl;
+    if(found==0){
+        cout<<"INVALID OUTPUT";
+        return 0;
+    }
+    int tot_mtr=0;
+    if(start<end){
+        for(int i=start+1;i<=end;i++){
+            tot_mtr+=path[i];
+        }
+    }
+    else if(start>end){
+        for(int i=start+1;i<length;i++){
+            tot_mtr+=path[i];
+        }
+        for(int i=0;i<=end;i++){
+            tot_mtr+=path[i];
+        }
+    }
+    cout<<tot_mtr<<endl;
+    cout<<setprecision(1)<<fixed<<ceil(tot_mtr*0.005)<<" INR";
+    cout.unsetf(std::ios::fixed);
+}
